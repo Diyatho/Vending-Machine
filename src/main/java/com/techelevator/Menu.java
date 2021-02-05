80046adf62175b4643f572f58bda5cd9741b8854
Dhakaram Kadarya <dhakaram29@gmail.com>
2021-02-05 09:36:11 -0500

    Menu class

--- src/main/java/com/techelevator/Menu.java
+++ src/main/java/com/techelevator/Menu.java
@@ -5,4 +5,13 @@ import com.techelevator.Inventory;
 
 public class Menu {
 
+	public void printMainMenu (){
+		System.out.println("> > (1) Display Vending Machine Items > (2) Purchase > (3) Exit >");
+		
+	}
+	
+	public void printPurchaseMenu () {
+		System.out.println(">>(1) Feed Money >(2) Select Product >(3) Finish Transaction > ");
+		
+	}
 }
